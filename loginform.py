2a130b586bf22856ef94b770e87b828f4d7a1761
Coconutrue <yanhodnev@yandex.ru>
2026-03-08 16:09:45 +0300

    first commit

---
+++ loginform.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, url_for, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('hello_world'), 303)
+    return render_template('login.html', title='Authorization', form=form)
+
+@app.route('/hello_world', methods=['GET', 'POST'])
+def hello_world():
+    return render_template('hello_world.html', title='Authorization')
+
+
+if __name__ == '__main__':
+    app.run(port=2010, host='127.0.0.1')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
