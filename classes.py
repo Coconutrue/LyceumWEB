a4379a9cfb81563172cded574a5171588166fa31
Coconutrue <yanhodnev@yandex.ru>
2026-03-09 10:09:36 +0300

    как будто все нормуль, но больше двух страниц почему-то сделать не получается тк они циклятся дра=уг с другом

---
+++ classes.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
+
+class homeForm(FlaskForm):
+    Lyceum = SubmitField('Lyceum?')
+
+class PerehodnikForm(FlaskForm):
+    Next = SubmitField('Next?')
+    Comeback = SubmitField('Comeback')
--- loginform.py
+++ loginform.py
@@ -1,18 +1,9 @@
 from flask import Flask, request, url_for, render_template, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from classes import LoginForm, homeForm, PerehodnikForm
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
 
-class PerehodnikForm(FlaskForm):
-    Next = SubmitField('Next?')
-    Comeback = SubmitField('Comeback')
+menu = [{"name": "Lyceum", "url": "perehodnik_lyceum"},
+        ]
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -22,15 +13,30 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for('perehodnik_lyceum'))
+        return redirect(url_for('home'))
     return render_template('login.html', title='Authorization', form=form)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    form = homeForm()
+    if form.validate_on_submit():
+        if form.Lyceum.data:
+            return redirect(url_for('perehodnik_lyceum'))
+    return render_template('home.html', title='Home', form=form)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def perehodnik_lyceum():
     form = PerehodnikForm()
     if form.validate_on_submit():
-        print("idi hahui")
+        if form.Next.data:
+            print("Нажата кнопка Next")
+            return "Выполняется действие для Next"
+        elif form.Comeback.data:
+            print("Нажата кнопка Comeback")
+            return "Выполняется действие для Comeback"
     return render_template('perehodnik_lyceum.html', title='COMEBACK', form=form)
+
 
 
 if __name__ == '__main__':
