05a297bf108ef0968de9d8a8967b7d53578a7a19
Coconutrue <yanhodnev@yandex.ru>
2026-03-22 19:13:38 +0300

    почти ничего не менялось

--- loginform.py
+++ loginform.py
@@ -7,7 +7,51 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+
 @app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.submit_reg.data:
+        if form.password_reg.data != form.confirm_password_reg.data:
+            return render_template('login.html', title='Authorization',
+                                   form=form,
+                                   message="Passwords don't match")
+        if not form.username_reg.data or not form.email_reg.data or not form.password_reg.data:
+            return render_template('login.html', title='Authorization',
+                                   form=form,
+                                   message="All registration fields must be filled in")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email_reg.data).first():
+            return render_template('login.html', title='Authorization',
+                                   form=form,
+                                   message="The user with this email already exists")
+        if db_sess.query(User).filter(User.name == form.username_reg.data).first():
+            return render_template('login.html', title='Authorization',
+                                   form=form,
+                                   message="A user with that name already exists")
+        user = User(
+            name=form.username_reg.data,
+            email=form.email_reg.data,
+        )
+        user.set_password(form.password_reg.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return render_template('login.html', title='Authorization',
+                               form=form,
+                               message="Successful registration, log in")
+    elif form.submit_log.data:
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email_log.data).first()
+        if user and user.check_password(form.password_log.data):
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', title='Authorization',
+                                   form=form,
+                                   message="Invalid email or password")
+
+    return render_template('login.html', title='Authorization', form=form)
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = homeForm()
@@ -17,51 +61,6 @@ def home():
         elif form.register.data:
             return redirect(url_for('login'))
     return render_template('home.html', title='Home', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.submit_reg.data:
-        if form.password_reg.data != form.confirm_password_reg.data:
-            return render_template('login.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        if not form.username_reg.data or not form.email_reg.data or not form.password_reg.data:
-            return render_template('login.html', title='Регистрация',
-                                   form=form,
-                                   message="Все поля регистрации должны быть заполнены")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email_reg.data).first():
-            return render_template('login.html', title='Регистрация',
-                                   form=form,
-                                   message="Пользователь с таким email уже существует")
-        if db_sess.query(User).filter(User.name == form.username_reg.data).first():
-            return render_template('login.html', title='Регистрация',
-                                   form=form,
-                                   message="Пользователь с таким именем уже существует")
-        user = User(
-            name=form.username_reg.data,
-            email=form.email_reg.data,
-        )
-        user.set_password(form.password_reg.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return render_template('login.html', title='Регистрация',
-                               form=form,
-                               message="Успешная регистрация, войдите в систему")
-    elif form.submit_log.data:
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email_log.data).first()
-        if user and user.check_password(form.password_log.data):
-            return redirect(url_for('home'))
-        else:
-            return render_template('login.html', title='Вход',
-                                   form=form,
-                                   message="Неверный email или пароль")
-
-    return render_template('login.html', title='Authorization', form=form)
-
 
 @app.route('/lyceum_home', methods=['GET', 'POST'])
 def lyceum_home():
