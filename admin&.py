903a8fed499de5da688602c3a79e026bb061a24b
Coconutrue <yanhodnev@yandex.ru>
2026-04-22 15:34:27 +0300

    убрал бесполезную фигню

--- admin&.py
+++ admin&.py
@@ -6,10 +6,8 @@ def make_admin(email):
     db_session.global_init("db/blogs.db")
     db_sess = db_session.create_session()
     user = db_sess.query(User).filter_by(email=email).first()
-
     if not user:
         print("нет такого пользователя. Прочекай бдшку")
-
     user.is_admin = True
     db_sess.commit()
     print("Админка накинута")
@@ -18,5 +16,4 @@ def make_admin(email):
 
 if __name__ == "__main__":
     email = input("поиск по почте. Вводи").strip()
-
     make_admin(email)
